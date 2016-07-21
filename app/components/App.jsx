f17ec7ef04a766759b69792212c8748e0b29f262
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-07-21 09:45:45 -0500

    add setState() to App

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -25,9 +25,31 @@ export default class App extends React.Component {
 
     return (
       <div>
-        <button onClick={() => console.log('add note')}>+</button>
+        <button onClick={this.addNote}>+</button>
         <Notes notes={notes} />
       </div>
     );
   }
+
+  addNote = () => {
+    // It would be possible to write this in an imperative style.
+    // I.e., through `this.state.notes.push` and then
+    // `this.setState({notes: this.state.notes})` to commit.
+    //
+    // I tend to favor functional style whenever that makes sense.
+    // Even though it might take more code sometimes, I feel
+    // the benefits (easy to reason about, no side effects)
+    // more than make up for it.
+    //
+    // Libraries, such as Immutable.js, go a notch further.
+    this.setState({
+      notes: this.state.notes.concat([{
+        id: uuid.v4(),
+        task: 'New task'
+      }])
+    });
+  }
+
+
+
 }
