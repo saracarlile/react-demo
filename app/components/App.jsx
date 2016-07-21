d9fb54d599799f1852193b135eae857c0bbd27de
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-07-21 09:19:55 -0500

    convert App to a class based component instead of a function

---
+++ app/components/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import uuid from 'uuid';
+import Notes from './Notes';
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+  render() {
+    const {notes} = this.state;
+
+    return (
+      <div>
+        <button onClick={() => console.log('add note')}>+</button>
+        <Notes notes={notes} />
+      </div>
+    );
+  }
+}
