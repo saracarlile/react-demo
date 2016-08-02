01426530e0ff466c8607cec52bb1f99f4e93d29f
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-08-02 16:45:59 -0500

    implementing edit of notes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -18,7 +17,7 @@ export default class App extends React.Component {
           task: 'Do laundry'
         }
       ]
-    };
+    }
   }
   render() {
     const {notes} = this.state;
@@ -26,11 +25,15 @@ export default class App extends React.Component {
     return (
       <div>
         <button onClick={this.addNote}>+</button>
-        <Notes notes={notes} onDelete={this.deleteNote} />
+        <Notes
+          notes={notes}
+          onNoteClick={this.activateNoteEdit}
+          onEdit={this.editNote}
+          onDelete={this.deleteNote}
+          />
       </div>
     );
   }
-
   addNote = () => {
     // It would be possible to write this in an imperative style.
     // I.e., through `this.state.notes.push` and then
@@ -49,13 +52,35 @@ export default class App extends React.Component {
       }])
     });
   }
-
   deleteNote = (id, e) => {
     // Avoid bubbling to edit
     e.stopPropagation();
 
     this.setState({
       notes: this.state.notes.filter(note => note.id !== id)
+    });
+  }
+  activateNoteEdit = (id) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = true;
+        }
+
+        return note;
+      })
+    });
+  }
+  editNote = (id, task) => {
+    this.setState({
+      notes: this.state.notes.map(note => {
+        if(note.id === id) {
+          note.editing = false;
+          note.task = task;
+        }
+
+        return note;
+      })
     });
   }
 }
