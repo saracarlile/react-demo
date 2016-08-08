fb790ac3458de55d4b6cf68c984fb8cbd609fccb
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-08-08 15:26:44 -0500

    set up localStorage for persisting the application state

--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -8,16 +8,7 @@ export default class NoteStore {
     this.bindActions(NoteActions);
 
 
-    this.notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+    this.notes = [];
   }
 
   create(note) {
