0ebb3e71410321121426fa0890ffab5eb0d59906
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-08-08 14:44:35 -0500

    porting add and delete to NoteActions

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import uuid from 'uuid';
 import Notes from './Notes';
 import connect from '../libs/connect';
+import NoteActions from '../actions/NoteActions';
 
 class App extends React.Component {
  
@@ -21,31 +22,18 @@ class App extends React.Component {
       </div>
     );
   }
-  addNote = () => {
-    // It would be possible to write this in an imperative style.
-    // I.e., through `this.state.notes.push` and then
-    // `this.setState({notes: this.state.notes})` to commit.
-    //
-    // I tend to favor functional style whenever that makes sense.
-    // Even though it might take more code sometimes, I feel
-    // the benefits (easy to reason about, no side effects)
-    // more than make up for it.
-    //
-    // Libraries, such as Immutable.js, go a notch further.
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
+  
+   addNote = () => {
+    this.props.NoteActions.create({
+      id: uuid.v4(),
+      task: 'New task'
     });
   }
   deleteNote = (id, e) => {
     // Avoid bubbling to edit
     e.stopPropagation();
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
+    this.props.NoteActions.delete(id);
   }
   activateNoteEdit = (id) => {
     this.setState({
--- app/stores/NoteStore.js
+++ app/stores/NoteStore.js
@@ -1,7 +1,13 @@
 import uuid from 'uuid';
+import NoteActions from '../actions/NoteActions';
+
 
 export default class NoteStore {
   constructor() {
+
+    this.bindActions(NoteActions);
+
+
     this.notes = [
       {
         id: uuid.v4(),
@@ -13,4 +19,19 @@ export default class NoteStore {
       }
     ];
   }
+
+  create(note) {
+    this.setState({
+      notes: this.notes.concat(note)
+    });
+  }
+  update(updatedNote) {
+    console.log('update note', updatedNote);
+  }
+  delete(id) {
+     this.setState({
+      notes: this.notes.filter(note => note.id !== id)
+    });
+  }
+
 }
